cafaaa8145e13f74735ee7bc7d43098628941de2
Civoremo <civoremon@gmail.com>
2021-08-25 10:02:17 -0400

    moralis badges

--- moralis-chat/src/components/login.js
+++ moralis-chat/src/components/login.js
@@ -4,6 +4,8 @@ import React, { useEffect } from "react";
 import { Moralis } from "moralis";
 import { useMoralis } from "react-moralis";
 import { useHistory } from "react-router-dom";
+import moralisBadge from "../assets/Powered-by-Moralis-Badge-Green.svg";
+import "boxicons";
 
 const Login = () => {
   let historyRedirect = useHistory();
@@ -28,11 +30,21 @@ const Login = () => {
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
-        border: "1px solid red",
+        // border: "1px solid red",
       }}
     >
-      <h1>Welcome to Moralis Chat: Weekly Challenge</h1>
-      <h3>Login to start chatting!</h3>
+      <a
+        href='https://github.com/Civoremo/moralis_learning/tree/main/moralis-chat'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <box-icon type='logo' name='github'></box-icon>
+      </a>
+      <h1>Moralis Chat: Weekly Challenge</h1>
+      <a href='https://moralis.io/' target='_blank' rel='noopener noreferrer'>
+        <img src={moralisBadge} alt='Moralis.io badge' />
+      </a>
+      <h3>Authenticate to start chatting!</h3>
       <button
         style={{ width: "100px", height: "50px" }}
         onClick={() => doAuthentication()}
